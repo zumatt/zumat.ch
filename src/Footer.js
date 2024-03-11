2cf53153f56e61f5709f0596b6fc398b374d2f5f
Matteo Subet <hi@zumat.ch>
2024-03-11 21:40:21 +0100

    Started development

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p><a href="mailto:hi@zumat.ch">Contact me</a> or discover more <a href="/about">about</a> who am I.</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/@ibm/plex/css/ibm-plex.css';
+import Home from './Home';
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Home />
+    <Footer />
   </React.StrictMode>
 );
 
